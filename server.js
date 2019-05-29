e21dd136615591589b97a9d45fd801e7d2f587e5
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-05-29 01:01:16 -0500

    Added basic server configuration

---
+++ server.js
@@ -0,0 +1,22 @@
+//Dependencies
+var express = require("express");
+var path = require("path");
+
+//EXpress Configuration
+var app = express();
+
+//Set initial port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//ROUTER
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//Listener
+app.listen(PORT, function(){
+    console.log ("Listening on Port:  " + PORT);
+});
