93b526139096fbbe44c8e34caab96c92ef177465
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-06-01 20:05:27 -0500

    edited  lines 16 and 17

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //ROUTER
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 //Listener
 app.listen(PORT, function(){
