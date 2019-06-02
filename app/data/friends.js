3bca539cdd187e48efc9cc55f71e8985cba8050f
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-06-01 22:35:01 -0500

    Added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,37 @@
 var friends = [
     {
-        name: "Harry Potter",
-        photo: "https://i.imgur.com/oSt58yz.jpg",
+        name: "Tom",
+        photo: "https://i.imgur.com/0fKhkkY.jpg",
         scores: [5, 1, 3, 4, 2, 2, 1, 5, 4, 3]
     },
     {
-        name: "Hermione Granger",
-        photo: "https://i.imgur.com/Lfa5giU.jpg",
+        name: "Scarlett",
+        photo: "https://i.imgur.com/oR4qtw0.jpg",
         scores: [5, 4, 5, 3, 2, 2, 1, 4, 1, 3]
     },
 
     {
-        name: "Ron weasley",
-        photo: "https://i.imgur.com/DGku9XP.jpg",
+        name: "Norman",
+        photo: "https://i.imgur.com/ps0IJUF.jpg",
         scores: [3, 4, 3, 4, 1, 1, 5, 5, 5, 1]
+    },
+
+    {
+        name: "Natalie",
+        photo: "https://i.imgur.com/4q6scdN.png",
+        scores: [3, 5, 3, 2, 4, 5, 5, 5, 5 ,5]
+    },
+
+    {
+        name: "Arnold",
+        photo: "https://i.imgur.com/Clrl1k4.jpg",
+        scores: [3, 5, 3, 2, 2, 4, 2, 1, 3 ,4]
+    },
+
+    {
+        name: "Maisie",
+        photo: "https://i.imgur.com/oz5vjl5.jpg",
+        scores: [3, 5, 3, 4, 5, 5, 3, 5, 3 ,4]
     }
 ];
 
