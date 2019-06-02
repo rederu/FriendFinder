d0403e13d77b651ee289ee331dc286fdd54ac9c9
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-06-01 20:05:55 -0500

    edited scores name

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = [
+    {
+        name: "Harry Potter",
+        photo: "https://i.imgur.com/oSt58yz.jpg",
+        scores: [5, 1, 3, 4, 2, 2, 1, 5, 4, 3]
+    },
+    {
+        name: "Hermione Granger",
+        photo: "https://i.imgur.com/Lfa5giU.jpg",
+        scores: [5, 4, 5, 3, 2, 2, 1, 4, 1, 3]
+    },
+
+    {
+        name: "Ron weasley",
+        photo: "https://i.imgur.com/DGku9XP.jpg",
+        scores: [3, 4, 3, 4, 1, 1, 5, 5, 5, 1]
+    }
+];
+
+//Export the array
+module.exports = friends;
